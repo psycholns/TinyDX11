fd8ed3fcde87046789ae2acb3eac5ece0b007a78
Henrik Münther <psycho@loonies.dk>
2021-09-04 15:37:37 +0200

    Updated for modern windows and compo rules
    No more D3DX, no fullscreen mode change and proper mouse cursor clear.  Newer crinkler almost saves the day keeping the size down.

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,20 @@
 #define WIDTH 1280
-#define HEIGHT 800                   
+#define HEIGHT 800                  
 
-
-// some tricks avoid the otherwise generated stubs - d3d11.h can not compile with the modified WINAPI definition
+// some tricks to avoid the otherwise generated stubs - d3d11.h/d3dcompiler.h can not compile with the modified WINAPI definition
 #define D3D11CreateDeviceAndSwapChain xxx
+#define D3DCompile yyy
+
 #include <d3d11.h>
+#include <d3dcompiler.h>
+
 #undef D3D11CreateDeviceAndSwapChain
+#undef D3DCompile
 #undef WINAPI
 #define WINAPI __declspec(dllimport)  __stdcall 
 
-#include <d3dx11.h>
-#include <stdio.h>
-
-extern "C" {
+extern "C" 
+{
 	// now with non-stub WINAPI
 	HRESULT WINAPI D3D11CreateDeviceAndSwapChain(
 		__in_opt IDXGIAdapter* pAdapter,
@@ -28,17 +30,37 @@ extern "C" {
 		__out_opt D3D_FEATURE_LEVEL* pFeatureLevel,
 		__out_opt ID3D11DeviceContext** ppImmediateContext);
 
-	// only need the direct pointer for this, for the uuid offset fetch
-	HRESULT __stdcall  _imp__D3DX11CompileFromMemory(LPCSTR pSrcData, SIZE_T SrcDataLen, LPCSTR pFileName, CONST D3D10_SHADER_MACRO* pDefines, LPD3D10INCLUDE pInclude,
-		LPCSTR pFunctionName, LPCSTR pProfile, UINT Flags1, UINT Flags2, ID3DX11ThreadPump* pPump, ID3D10Blob** ppShader, ID3D10Blob** ppErrorMsgs, HRESULT* pHResult);
+	HRESULT WINAPI
+		D3DCompile(_In_reads_bytes_(SrcDataSize) LPCVOID pSrcData,
+			_In_ SIZE_T SrcDataSize,
+			_In_opt_ LPCSTR pSourceName,
+			_In_reads_opt_(_Inexpressible_(pDefines->Name != NULL)) CONST D3D_SHADER_MACRO* pDefines,
+			_In_opt_ ID3DInclude* pInclude,
+			_In_opt_ LPCSTR pEntrypoint,
+			_In_ LPCSTR pTarget,
+			_In_ UINT Flags1,
+			_In_ UINT Flags2,
+			_Out_ ID3DBlob** ppCode,
+			_Always_(_Outptr_opt_result_maybenull_) ID3DBlob** ppErrorMsgs);
 };
 
 
-// just a hlsl adaption of "No Chrome Spheres Permitted" - could be optimized further, but that's not the point
+// a fullscreen window with stretched content is more compatible than oldskool fullscreen mode these days
+static int SwapChainDesc[] = {
+	WIDTH, HEIGHT, 0, 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 0,  // w,h,refreshrate,format, scanline, (fullscreen)scaling
+	1, 0, // sampledesc (count, quality)
+	DXGI_USAGE_UNORDERED_ACCESS, // usage
+	1, // buffercount
+	0, // hwnd
+	1, 0, 0 }; // windowed, swap_discard, flags
+
+static D3D11_BUFFER_DESC ConstBufferDesc = { 256, D3D11_USAGE_DEFAULT, D3D11_BIND_CONSTANT_BUFFER, 0, 0, 0 }; // 256,0,4,0,0,0
+
+// just a hlsl adaption of "No Chrome Spheres Permitted" to have something
 static char shader[] =
 "int t;"  // implicit constant buffer
-"RWTexture2D<float4> y;"
-"[numthreads(16,16,1)]"
+"RWTexture2D<float3> y;"
+"[numthreads(8,8,1)]"
 "void cs_5_0(uint3 i:sv_dispatchthreadid)"
 "{"
 "float3 v=i/float3(640,400,1)-1,"
@@ -52,37 +74,25 @@ static char shader[] =
 "}"
 "if(w.y<0){"
 "p-=w*(p.y+1)/w.y;"
-"if(sin(p.z*6)*sin(p.x*6)>0)x+=2/length(p);"
+"x+=(sin(p.z*6)*sin(p.x*6)>0)*2/length(p);"
 "}"
-"y[i.xy]=(abs(v.y-v.x)>.1&&abs(v.y+v.x)>.1)?x:float4(1,0,0,0);"
+"y[i.xy]=abs(v.y-v.x)>.1&&abs(v.y+v.x)>.1?x:float3(1,0,0);"
 "}";
 
-static int SwapChainDesc[] = {
-	WIDTH, HEIGHT, 0, 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 0,  // w,h,refreshrate,format, scanline, scaling
-	1, 0, // sampledesc (count, quality)
-	DXGI_USAGE_UNORDERED_ACCESS, // usage
-	1, // buffercount
-	0, // hwnd
-	0, 0, 0 }; // fullscreen, swap_discard, flags
-
-static D3D11_BUFFER_DESC ConstBufferDesc = { 16, D3D11_USAGE_DEFAULT, D3D11_BIND_CONSTANT_BUFFER, 0, 0, 0 }; // 16,0,4,0,0,0
 
 #ifndef _DEBUG
 void WinMainCRTStartup()
 {
-	//note: to remove cursor under newer windows you need both the decoration bits and showcursor, like this:
-	//HWND  hWnd = CreateWindowExA(0,(LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0); //"edit"
-	//ShowCursor(0);
-
-	HWND  hWnd = CreateWindowExA(0, (LPCSTR)0xC019, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); //"static"
+	//note: to remove cursor under newer windows you need both the decoration bits and showcursor
+	ShowCursor(0);
+	HWND  hWnd = CreateWindowExA(0,(LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0); //"edit"
 #else
+#include <stdio.h>
 int WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nFunsterStil)
 {
 	HWND hWnd = CreateWindowA("edit", 0, WS_POPUP | WS_VISIBLE, 0, 0, WIDTH, HEIGHT, 0, 0, 0, 0);
-	SwapChainDesc[12] = 1; // windowed
 #endif
 	SwapChainDesc[11] = (int)hWnd;
-
 
 	// setting up device
 	ID3D11Device*               pd3dDevice;
@@ -92,13 +102,11 @@ int WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument
 	HRESULT hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_DEBUG, 0, 0, D3D11_SDK_VERSION, (DXGI_SWAP_CHAIN_DESC*)&SwapChainDesc[0], &pSwapChain, &pd3dDevice, NULL, &pImmediateContext);
 	if (FAILED(hr)) return hr;
 #else
-	D3D11CreateDeviceAndSwapChain( NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, 0,0,D3D11_SDK_VERSION, (DXGI_SWAP_CHAIN_DESC*)&SwapChainDesc[0], &pSwapChain, &pd3dDevice, NULL, &pImmediateContext );
+	D3D11CreateDeviceAndSwapChain( NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, 0,0, D3D11_SDK_VERSION, (DXGI_SWAP_CHAIN_DESC*)&SwapChainDesc[0], &pSwapChain, &pd3dDevice, NULL, &pImmediateContext );
 #endif
 
-
 	// unordered access view on back buffer
-	IID* iid = (IID*)(*(char**)_imp__D3DX11CompileFromMemory - 36181);   // superdirty way to get that otherwise big guid - d3dx11_43 will remain constant, so it's ok...
-	pSwapChain->GetBuffer(0, /*__uuidof( ID3D11Texture2D )*/*iid, (LPVOID*)&SwapChainDesc[0]);	// reuse swapchain struct for temp storage of texture pointer
+	pSwapChain->GetBuffer(0, __uuidof( ID3D11Texture2D ), (LPVOID*)&SwapChainDesc[0]);	// reuse swapchain struct for temp storage of texture pointer
 	pd3dDevice->CreateUnorderedAccessView((ID3D11Texture2D*)SwapChainDesc[0], NULL, (ID3D11UnorderedAccessView**)&SwapChainDesc[0]);
 	pImmediateContext->CSSetUnorderedAccessViews(0, 1, (ID3D11UnorderedAccessView**)&SwapChainDesc[0], 0);
 
@@ -112,7 +120,8 @@ int WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument
 #ifdef _DEBUG
 	ID3D10Blob* pErrorBlob = NULL;
 	ID3D10Blob* pBlob = NULL;
-	hr = D3DX11CompileFromMemory(shader, sizeof(shader), 0, 0, 0, "cs_5_0", "cs_5_0", D3D10_SHADER_DEBUG, 0, 0, &pBlob, &pErrorBlob, 0);
+	hr = D3DCompile(shader, sizeof(shader), 0, 0, 0, "cs_5_0", "cs_5_0", D3D10_SHADER_DEBUG, 0, &pBlob, &pErrorBlob);
+
 	if (FAILED(hr))
 	{
 		char message[4096];
@@ -130,19 +139,22 @@ int WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument
 	if (pBlob != 0) pBlob->Release();
 #else
 	ID3D10Blob* pBlob;
-	D3DX11CompileFromMemory(shader, 1024, 0, 0, 0, "cs_5_0", "cs_5_0", 0, 0, 0, &pBlob, 0, 0); 
+	D3DCompile(shader, 1024, 0, 0, 0, "cs_5_0", "cs_5_0", 0, 0, &pBlob, 0); // We can use some abitary large value for size, null terminated anyway...
+
 	pd3dDevice->CreateComputeShader((void*)(((int*)pBlob)[3]), ((int*)pBlob)[2], NULL, &pCS);
 #endif
 	pImmediateContext->CSSetShader(pCS, NULL, 0);
 
-
 	do
 	{
-		SwapChainDesc[0] = GetTickCount();
-		pImmediateContext->UpdateSubresource(pConstants, 0, 0, &SwapChainDesc[0], 4, 4);
-		pImmediateContext->Dispatch(WIDTH / 16, HEIGHT / 16, 1);
+		PeekMessageA(0, 0, 0, 0, PM_REMOVE); // unanswered input may kill you - at least in windowed mode
+		
+		SwapChainDesc[0] = GetTickCount();   // please use something better (from the music), this is too coarse..
+		pImmediateContext->UpdateSubresource(pConstants, 0, 0, &SwapChainDesc[0], 0, 0);
+		pImmediateContext->Dispatch(WIDTH / 8, HEIGHT / 8, 1);
 		pSwapChain->Present(0, 0);
-	} while (!GetAsyncKeyState(VK_ESCAPE));
+	} 
+	while (!GetAsyncKeyState(VK_ESCAPE));
 
-	ExitProcess(0);
+	ExitProcess(0);  // actually not needed in this setup, but may be smaller..
 }
